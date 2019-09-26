2e41f2b0c4edb0d8dbbe0b4cdcf95ed5646b6cb2
Silvio Mayolo <sdmayolo42@students.tntech.edu>
2019-09-26 10:23:53 -0400

    If units are passed implicitly, the units will remain in the answer

--- src/Data/Calc/Function/Unit.hs
+++ src/Data/Calc/Function/Unit.hs
@@ -1,5 +1,5 @@
 
-module Data.Calc.Function.Unit where
+module Data.Calc.Function.Unit(uconvert) where
 
 import Data.Calc.Unit.Table
 import Data.Calc.Unit.Type
@@ -12,17 +12,38 @@ import Prelude hiding (fail)
 import Control.Monad.Reader hiding (fail)
 import Control.Monad.Fail
 
--- This is a cheap and dirty covert function. It's just for me to test
--- things right now.
+data ConvertArgForm = ArgsImplicit | ArgsExplicit
+                      deriving (Show, Read, Eq, Ord, Enum)
+
+-- To clarify, uconvert accepts two forms.
 --
--- TODO Clean it up and put it somewhere under Data.Calc.Function.*
+-- Explicit arguments:
+--
+--   uconvert(3, in, ft)
+--
+-- This will convert 3 from inches to feet and produce a number as a
+-- result.
+--
+-- Implicit arguments:
+--
+--   uconvert(3 * in, ft)
+--
+-- This will do the same, but rather than simply producing the result
+-- N as a number, it will return N * ft (keeping the units tagged with
+-- the number).
+
 uconvert :: Monad m => Function m
 uconvert = function "uconvert" go
     where go = do
-            (expr, old, new) <- ask >>= parseArgs
+            (form, expr, old, new) <- ask >>= parseArgs
             old' <- parseUnits table old
             new' <- parseUnits table new
-            maybeToFail $ convert old' new' expr
-          parseArgs [expr, old, new] = pure (expr, old, new)
-          parseArgs [old, new] = pure (Constant (PrimNum 1), old, new)
+            result <- maybeToFail $ convert old' new' expr
+            return $ case form of
+                       ArgsExplicit -> result
+                       -- If arguments are passed "implicitly", include the
+                       -- resulting units in the result
+                       ArgsImplicit -> Compound "*" [result, new]
+          parseArgs [expr, old, new] = pure (ArgsExplicit, expr, old, new)
+          parseArgs [old, new] = pure (ArgsImplicit, Constant (PrimNum 1), old, new)
           parseArgs _ = fail "invalid args to uconvert"
